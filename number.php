9512c51c1719d5ccac728cda3f14c71fcb55d7e9
PragonMedia <tech@paragonmedia.io>
2025-11-19 07:29:44 +0800

    Update number.php

--- number.php
+++ number.php
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 }
 
 // For now, hardcoded phone number - in the future this will make an API call to a server
-$phoneNumber = "18664982822";
+$phoneNumber = "13213980346";
 
 // Return the phone number as JSON
 echo json_encode([
